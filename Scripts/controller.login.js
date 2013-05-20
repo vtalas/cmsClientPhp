e28332d44420931abdc4c0def6179a8bfda7a1b2
v.talas <v.talas@gmc.net>
2013-05-20 12:03:59 +0200

    login page

---
+++ Scripts/controller.login.js
@@ -0,0 +1,13 @@
+function loginController($scope, cmsApi) {
+	var api = new ApiWrapper(cmsApi);
+
+	console.log("kjbasdjkbas")
+
+//	api.getPages().then(function (data) {
+//   		$scope.pages = data;
+//		console.log(data)
+//	});
+
+
+
+}
