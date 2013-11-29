bb5360ee224235f5a13b4b717ebc70a9cb4ed9ac
unknown <v.talas@TALAS326.cz.gmc.net>
2013-11-29 16:09:17 +0100

    build

---
+++ _tools/build.bat
@@ -0,0 +1,14 @@
+
+type ..\common\*.js > ../build/app.js
+type ..\gallery\*.js >> ../build/app.js
+type ..\api\*.js >> ../build/app.js
+type ..\data\*.js >> ../build/app.js
+type ..\Scripts\*.js >> ../build/app.js
+
+
+
+type ..\css\*.css >> ../build/app.css
+
+type ..\libs\css\font-awesome\css\*.min.css >> ../build/app.libs.css
+type ..\libs\css\font\*.css >> ../build/app.libs.css
+
