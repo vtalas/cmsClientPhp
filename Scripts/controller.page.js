001107ed81fb4f524e2491851d71df03e4621267
v.talas <v.talas@gmc.net>
2014-02-05 12:01:07 +0100

    refactoring pro Get json

--- Scripts/controller.page.js
+++ Scripts/controller.page.js
@@ -1,7 +1,36 @@
-var pageController = ["$scope", "$api", "$routeParams", "$gallery", "$notify", "$timeout", function ($scope, $api, $routeParams, $gallery, $notify, $timeout) {
-	var source = null;
+var pageController = ["$scope", "$api", "$routeParams", "$gallery", "$notify", "$timeout", "loadGridList", function ($scope, $api, $routeParams, $gallery, $notify, $timeout, loadGridList) {
+	var source = null,
+		/** type GridList */
+			gridList;
 
 	$scope.link = $routeParams.link;
+
+
+	loadGridList
+		.then(function (data) {
+			gridList = data;
+
+	//$timeout(function () {
+			$scope.page = gridList.getGridByLink($scope.link);
+			$scope.gridElements = $scope.page.GridElements;
+			$scope.groups = $scope.page.groups;
+
+			$scope.layoutClass = $scope.groups ? "grid_10" : "grid_12";
+			$notify.trigger("content-loaded");
+			source = new GridElementsList($scope.page.GridElements);
+			$gallery.loadData($scope.gridElements || []);
+			return $scope.page;
+//},2000)
+
+		}, function (err) {
+			console.log("ERROR!!", err.status);
+		})
+		.then(function (data) {
+			setTimeout(function () {
+				$api.checkForSnapshot($scope, data);
+			}, 3000);
+		});
+
 
 	$notify.trigger("content-loading");
 	$api.getPage($scope.link)
